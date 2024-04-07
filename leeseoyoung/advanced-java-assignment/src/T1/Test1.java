2675b30381ae3a16c8b548b47d22d675d163deef
leeseoyoung05 <lsy202415004@gmail.com>
2024-04-08 08:04:38 +0900

    2주차 assignment - 이서영

--- leeseoyoung/advanced-java-assignment/src/T1/Test1.java
+++ leeseoyoung/advanced-java-assignment/src/T1/Test1.java
@@ -4,18 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
+
 public class Test1 {
+
     public static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) {
         list = Arrays.asList(1, 2, 3, 4, 5, 6);
 
         double avg = list.stream()
-                .mapToDouble(number-> Math.sqrt(number))
+                .mapToDouble(Math::sqrt)
                 .average()
-                        .orElseThrow(NoSuchElementException::new);
+                .orElseThrow(NoSuchElementException::new);
+
         System.out.println(avg);
-            }
-        }
-
-
+    }
+}
