1dd00c371005b7711c2449f082b13e46999d91a2
firefox1234123 <77014582+firefox1234123@users.noreply.github.com>
2024-04-08 01:06:18 +0900

    Update Test2.java

--- dayeon/advanced-java-assignment/src/T2/Test2.java
+++ dayeon/advanced-java-assignment/src/T2/Test2.java
@@ -10,11 +10,10 @@ public class Test2 {
 
     public static void main(String[] args) {
         double ans1 = action((array -> {
-            double result = 0;
-            for (int i = 1; i <= array.length; i++) {
-                if (array[(int)result] > array[i - 1]) { result = i; }
-                else { result = i - 1; }
-            } return array[(int)result];
+            double max = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (max < array[i]) { max = array[i]; }
+            } return max;
         }));
         System.out.println(ans1);
 
