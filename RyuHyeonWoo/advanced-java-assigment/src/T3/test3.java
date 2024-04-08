884f259edbe57b2ba713312372c62a0aa58e5439
HyeonWooRyu213 <fbgusdn05@gmail.com>
2024-04-09 03:27:22 +0900

    2주차 과제

--- RyuHyeonWoo/advanced-java-assigment/src/T3/test3.java
+++ RyuHyeonWoo/advanced-java-assigment/src/T3/test3.java
@@ -12,14 +12,15 @@ public class test3 {
         try {
             String[] number = inputNumber.split("\\s+");
             int sum = 0;
-            for (String numbers : number) {
-                sum += Integer.parseInt(numbers);
+            for (int i = 0; i < count; i++) {
+                sum += Integer.parseInt(number[i]);
             }
             System.out.println("정수들의 합은 " + sum);
         } catch (NumberFormatException e) {
             System.out.println("예외가 발생했습니다!");
             System.out.println("예외 정보: " + e);
         }
+        scanner.close();
     }
     scanner.close();
 }
--- RyuHyeonWoo/database/Assigment.sql
+++ RyuHyeonWoo/database/Assigment.sql
@@ -1,31 +1,26 @@
 ﻿CREATE TABLE `card` (
-	`cardNumber`	String	NOT NULL,
-	`cvc`	VARCHAR(255)	NULL,
-	`cardType`	VARCHAR(255)	NULL,
-	`bankConnect`	VARCHAR(255)	NULL
+	`cardNumber`	VARCHAR(255)	NOT NULL,
+	`cvc`	VARCHAR(255)	NOT NULL,
+	`cardType`	VARCHAR(255)	NOT NULL,
+	`bankConnect`	VARCHAR(255)	NOT NULL
 );
 
-CREATE TABLE `user` (
-	`id`	String	NOT NULL,
-	`cardNumber`	String	NOT NULL,
-	`name`	VARCHAR(255)	NULL,
-	`age`	VARCHAR(255)	NULL,
-	`accountNumber`	VARCHAR(255)	NULL
+CREATE TABLE `auser` (
+	`name`	VARCHAR(255)	NOT NULL,
+	`age`	VARCHAR(255)	NOT NULL,
+	`accountNumber`	VARCHAR(255)	NOT NULL,
+	`cardNumber`	VARCHAR(255)	NOT NULL,
+	`id`	VARCHAR(255)	NOT NULL
 );
 
 CREATE TABLE `bank` (
-	`id`	String	NOT NULL,
-	`bankBook`	VARCHAR(255)	NULL,
-	`vipGrade`	VARCHAR(255)	NULL,
-	`password`	VARCHAR(255)	NULL
+	`id`	VARCHAR(255)	NOT NULL,
+	`bankNumber`	VARCHAR(255)	NOT NULL,
+	`vipGrade`	VARCHAR(255)	NOT NULL,
+	`password`	VARCHAR(255)	NOT NULL
 );
 
 ALTER TABLE `card` ADD CONSTRAINT `PK_CARD` PRIMARY KEY (
-	`cardNumber`
-);
-
-ALTER TABLE `user` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
-	`id`,
 	`cardNumber`
 );
 
@@ -33,31 +28,17 @@ ALTER TABLE `bank` ADD CONSTRAINT `PK_BANK` PRIMARY KEY (
 	`id`
 );
 
-ALTER TABLE `user` ADD CONSTRAINT `FK_bank_TO_user_1` FOREIGN KEY (
-	`id`
-)
-REFERENCES `bank` (
-	`id`
-);
-
-ALTER TABLE `user` ADD CONSTRAINT `FK_card_TO_user_1` FOREIGN KEY (
-	`cardNumber`
-)
-REFERENCES `card` (
-	`cardNumber`
-);
-
-INSERT INTO 'card'(cardNumber, cvc, cardType, bankConnect) VALUES
+INSERT INTO card(cardNumber, cvc, cardType, bankConnect) VALUES
 ('1242-43-5436', '213', 'CHECK', 'KBSTAR'),
 ('4988-93-2966', '052', 'CHECK', 'SHINHAN'),
-('5461-00-8944', '567', 'CREDIT', 'KBSTER');
+('5461-00-8944', '567', 'CREDIT', 'KBSTAR');
 
-INSERT INTO 'bank'(id, bankBook, vipGrade, password) VALUES
-('KIM312', '1412-6548', 'NORMAL', '5347'),
+INSERT INTO bank(id, bankNumber, vipGrade, password) VALUES
+('KIM312', '1412-6548', 'NARMAL', '5347'),
 ('LEE124', '1205-5967', 'VIP', '9045'),
 ('PARK822', '5478-1238', 'VVIP', '1588');
 
-INSERT INTO 'user'(name, age, accountNumber) VALUES
-('PARK822', '42', '177473'),
-('PARK822', '38', '138905'),
-('PARK822', '58', '546458');
+INSERT INTO auser(name, age, accountNumber, cardNumber, id) VALUES
+('PARK822', '42', '177473', '1242-43-5436', 'KIM312'),
+('PARK822', '38', '138905', '4988-93-2966', 'LEE124'),
+('PARK822', '58', '546458', '5461-00-8944', 'PARK822');
